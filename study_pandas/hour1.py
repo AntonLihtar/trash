2afe0419e1be504a68ee1620113f1de3b8034cf9
ant1 <l1htar@bk.ru>
2025-10-06 08:42:18 +0300

    init bro

---
+++ study_pandas/hour1.py
@@ -0,0 +1,6 @@
+def go_start_pd():
+    print('hello  pandus')
+
+
+if __name__ == '__main__':
+    go_start_pd()
---
+++ various/test.py
@@ -0,0 +1,6 @@
+def go_start_test():
+    print('hello  bro')
+
+
+if __name__ == '__main__':
+    go_start_test()
